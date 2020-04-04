72c4de7ee17bf9bb523c75b610677c2ee51accc9
Siddhant Chothe <sidnc86@gmail.com>
2020-04-04 06:53:40 +0530

    Decrease number of calls made to webDriver.findElement(getBy())
    A WebElement is assigned at class level right at constructor time.
    This way methods like getAttribute(), getTagName(), etc. can directly use this.element instance.
    There is no need to find WebElement again and again whenever getAttribute() or any such method is called.

--- src/test/java/webelement/customElements/superElements/CustomWebElement.java
+++ src/test/java/webelement/customElements/superElements/CustomWebElement.java
@@ -30,6 +30,17 @@ public abstract class CustomWebElement {
     private WebElementTransformer transformer;
 
     /**
+     * Used to store a WebElement so that driver.findElement() isn't called repetatively
+     */
+    protected WebElement element;
+
+    /**
+     * Used to store list index of WebElement within a List<WebElement>
+     * Can be used to determine getBy() with this index
+     */
+    private int listIndex;
+
+    /**
      * Constructor.
      *
      * @param webDriver The webDriver used to interact with the webbrowser.
@@ -39,6 +50,26 @@ public abstract class CustomWebElement {
         this.webDriver = webDriver;
         locator = by;
         transformer = new WebElementTransformer();
+        element = this.webDriver.findElement(locator);
+
+        // Call the page factory on this object to initialize custom webelements in custom webelements (aka nesting)
+        PageFactory.initElements(new CustomElementFieldDecorator(webDriver, webDriver), this);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param webDriver The webDriver used to interact with the webbrowser.
+     * @param by        The locator used to identify the element(s) on the website. This is expected to return List<WebElement>
+     * @param webElement The webElement which forms part of List<WebElement>
+     * @param listIndex The list index of webElement within the List<WebElement>
+     **/
+    public CustomWebElement(WebDriver webDriver, By by, WebElement webElement, int listIndex) {
+        this.webDriver = webDriver;
+        locator = by;
+        transformer = new WebElementTransformer();
+        element = webElement;
+        this.listIndex = listIndex;
 
         // Call the page factory on this object to initialize custom webelements in custom webelements (aka nesting)
         PageFactory.initElements(new CustomElementFieldDecorator(webDriver, webDriver), this);
@@ -51,6 +82,14 @@ public abstract class CustomWebElement {
      **/
     public By getBy() {
         return locator;
+    }
+
+    /**
+     * Returns the list index within a List<WebElement>
+     * @return listIndex
+     */
+    public int getListIndex() {
+    	return this.listIndex;
     }
 
     /**
@@ -81,7 +120,7 @@ public abstract class CustomWebElement {
      * @return The attribute/property's current value or null if the value is not set.
      */
     public String getAttribute(String attributeName) {
-        return getWebDriver().findElement(getBy()).getAttribute(attributeName);
+        return element.getAttribute(attributeName);
     }
 
     /**
@@ -121,17 +160,19 @@ public abstract class CustomWebElement {
         return transformer.getLocatorValue(getBy(), type);
     }
     
-    /*
-     * This two method we have defined for use CustomWebElement properly.
+    /**
+     * Returns the tag name
+     * 
+     *   @return tag name
      * */
     public String getTagName()
     {
-    	return webDriver.findElement(getBy()).getTagName();
+    	return element.getTagName();
     }
     
     public void sendKeys(Keys keys)
     {
-    	webDriver.findElement(getBy()).sendKeys(keys);
+    	element.sendKeys(keys);
     }
     
     /**
@@ -141,15 +182,15 @@ public abstract class CustomWebElement {
      * Clicks on the button.
      **/
     public void click() {
-        getWebDriver().findElement(getBy()).click();
+        element.click();
     }
 
     /**
      * Sets the text of the element.
      **/
     public void setText(String text) {
-        getWebDriver().findElement(getBy()).clear();
-        getWebDriver().findElement(getBy()).sendKeys(text);
+        element.clear();
+        element.sendKeys(text);
     }
 
     /**
@@ -158,7 +199,7 @@ public abstract class CustomWebElement {
      * @return The found sub web element of this complex web element.
      **/
     public WebElement findElement(By locator) {
-        return getWebDriver().findElement(getBy()).findElement(locator);
+        return element.findElement(locator);
     }
 
     /**
@@ -167,7 +208,7 @@ public abstract class CustomWebElement {
      * @return The found sub web elements of this complex web element.
      **/
     public List<WebElement> findElements(By locator) {
-        return getWebDriver().findElement(getBy()).findElements(locator);
+        return element.findElements(locator);
     }
 
     /**
@@ -176,21 +217,21 @@ public abstract class CustomWebElement {
      * @return Returns the node text of the element.
      **/
     public String getText() {
-        return getWebDriver().findElement(getBy()).getText();
+        return element.getText();
     }
     
     public boolean isSelected()
     {
-    	return getWebDriver().findElement(getBy()).isSelected();
+    	return element.isSelected();
     }
     
     public boolean isDisplayed()
     {
-    	return getWebDriver().findElement(getBy()).isDisplayed();
+    	return element.isDisplayed();
     }
     
     public boolean isEnabled()
     {
-    	return getWebDriver().findElement(getBy()).isEnabled();
+    	return element.isEnabled();
     }
 }
